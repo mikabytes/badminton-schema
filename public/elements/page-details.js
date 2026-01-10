d48a34ddb64fa7bcca1d8ea7ff64c9764b77daea
mikabytes <1054229+mikabytes@users.noreply.github.com>
2026-01-10 18:51:38 +0100

    enhance page-details

--- public/elements/page-details.js
+++ public/elements/page-details.js
@@ -7,6 +7,7 @@ import responses from "features/responses.js"
 import skips from "features/skips.js"
 import rules from "features/rules.js"
 import error from "./error.js"
+import generateEvents from "../events.js"
 
 class PageDetails extends Element {
   static css = new URL(`./page-details.css`, import.meta.url)
@@ -17,19 +18,54 @@ class PageDetails extends Element {
   }
 
   disconnectedCallback() {
-    super.disconnectedCallback() 
+    super.disconnectedCallback()
     this.removeEventListener(`click`, this.goback)
   }
 
   render() {
-    let err = error(rules, skips, users, responses) 
+    let err = error(rules, skips, users, responses)
+
+    if (err) {
+      return err
+    }
+
+    const eventId = page.value.item
+    const date = eventId * 60000
+
+    const events = generateEvents(
+      rules.value,
+      skips.value.map((d) => d.date),
+      new Date(date),
+      new Date(date)
+    )
+
+    if (events.length !== 1) {
+      return html`Invalid event.`
+    }
+
+    const event = events[0]
 
     this.setActions(html`<button @click=${this.remove}>Ta bort</button>`)
 
-    return html` 
-      ${err ? err : html``}
+    return html`
+      <h1>${capitalizeFirst(formatter.format(event.date))}</h1>
+      <hr />
+      <pre>${JSON.stringify(event)}</pre>
     `
   }
 }
 
 customElements.define("x-page-details", PageDetails)
+
+function capitalizeFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
+const formatter = new Intl.DateTimeFormat("sv-SE", {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+  hour: "2-digit",
+  minute: "2-digit",
+})
